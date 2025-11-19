4db3158fc79c6af25eb0c4da0548c0e1a2ce4073
armelyara <theday.ay@gmail.com>
2025-11-18 20:43:05 -0500

    Update

---
+++ public/js/config.js
@@ -0,0 +1,26 @@
+// Configuration Firebase 
+const firebaseConfig = {
+    apiKey: "AIzaSyBgjOaHX_B5Q0oVkpHD1wOOUJ-aIPZT85E",
+    authDomain: "pubdev-71378.firebaseapp.com",
+    projectId: "pubdev-71378",
+    storageBucket: "pubdev-71378.firebasestorage.app",
+    messagingSenderId: "1077633712174",
+    appId: "1:1077633712174:web:dad17ac3ec8f3a3299dfbf",
+    measurementId: "G-2ELG3TYF8Z"
+};
+
+// Configuration de l'application
+const APP_CONFIG = {
+    name: 'PublieDev',
+    version: '1.0.0',
+    pageSize: 12,
+    maxSearchResults: 50,
+    supportedTypes: ['app', 'api', 'program', 'tutorial', 'article'],
+    typeLabels: {
+        app: 'Application',
+        api: 'API',
+        program: 'Programme',
+        tutorial: 'Tutoriel',
+        article: 'Article'
+    }
+};
